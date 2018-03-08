d53fc2c551e33d27e946f59c761c8773c94033af
Victor Y. Fadeev <victor.y.fadeev@gmail.com>
2018-03-08 18:22:53 +0300

    Fix Data types

--- Differential_Equation/src/Data.c
+++ Differential_Equation/src/Data.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 
-typedef struct tVector
+typedef struct Vector
 {
 	double a;
 	double b;
@@ -13,37 +13,165 @@ typedef struct tVector
 	double y0;
 } Vector;
 
-typedef struct tInterval
+typedef struct Interval
 {
-	int quantity_of_points;
+	int steps;
+	double distance;
+	int points;
 	double h;
-	int quantity_of_jumps;
-	double distance_jump;
 } Interval;
 
-Vector* create_vector(double a, double b, double c, double alpha, double beta, double x0, double y0)
+
+Vector *create_vector(const double a
+					, const double b
+					, const double c
+					, const double alpha
+					, const double beta
+					, const double x0
+					, const double y0)
 {
 	Vector *vector = malloc(sizeof(Vector));
-	set_a(vector,a);
-	set_b(vector,b);
-	set_c(vector,c);
-	set_alpha(vector,alpha);
-	set_beta(vector,beta);
-	set_x0(vector,x0);
-	set_y0(vector,y0);
+
+	vector->a = a;
+	vector->b = b;
+	vector->c = c;
+	vector->alpha = alpha;
+	vector->beta = beta;
+	vector->x0 = x0;
+	vector->y0 = y0;
+
 	return vector;
 }
 
-
-Interval *create_interval(int quantity_of_points, double h, int quantity_of_jumps, double distance_jump)
+Interval *create_interval(const int steps
+						, const double distance
+						, const int points
+						, const double h)
 {
 	Interval *interval = malloc(sizeof(Interval));
-	set_quantity_of_points(interval, quantity_of_points);
-	set_h(interval, h);
-	set_quantity_of_jumps(interval, quantity_of_jumps);
-	set_distance_jump(interval, distance_jump);
+
+	interval->steps = steps;
+	interval->distance = distance;
+	interval->points = points;
+	interval->h = h;
+
 	return interval;
 }
+
+
+const double get_a(const Vector *vector)
+{
+	return vector->a;
+}
+
+const double get_b(const Vector *vector)
+{
+	return vector->b;
+}
+
+const double get_c(const Vector *vector)
+{
+	return vector->c;
+}
+
+const double get_alpha(const Vector *vector)
+{
+	return vector->alpha;
+}
+
+const double get_beta(const Vector *vector)
+{
+	return vector->beta;
+}
+
+const double get_x0(const Vector *vector)
+{
+	return vector->x0;
+}
+
+const double get_y0(const Vector *vector)
+{
+	return vector->y0;
+}
+
+
+const int get_steps(const Interval *interval)
+{
+	return interval->steps;
+}
+
+const double get_distance(const Interval *interval)
+{
+	return interval->distance;
+}
+
+const int get_points(const Interval *interval)
+{
+	return interval->points;
+}
+
+const double get_h(const Interval *interval)
+{
+	return interval->h;
+}
+
+
+void set_a(Vector *vector, const double a)
+{
+	vector->a = a;
+}
+
+void set_b(Vector *vector, const double b)
+{
+	vector->b = b;
+}
+
+void set_c(Vector *vector, const double c)
+{
+	vector->c = c;
+}
+
+void set_alpha(Vector *vector, const double alpha)
+{
+	vector->alpha = alpha;
+}
+
+void set_beta(Vector *vector, const double beta)
+{
+	vector->beta = beta;
+}
+
+void set_x0(Vector *vector, const double x0)
+{
+	vector->x0 = x0;
+}
+
+void set_y0(Vector *vector, const double y0)
+{
+	vector->y0 = y0;
+}
+
+
+void set_steps(Interval *interval, const int steps)
+{
+	interval->steps = steps;
+}
+
+void set_distance(Interval *interval, const double distance)
+{
+	interval->distance = distance;
+}
+
+void set_points(Interval *interval, const int points)
+{
+	interval->points = points;
+}
+
+void set_h(Interval *interval, const double h)
+{
+	interval->h = h;
+}
+
 
 void delete_vector(Vector *vector)
 {
@@ -53,114 +181,4 @@ void delete_vector(Vector *vector)
 void delete_interval(Interval *interval)
 {
 	free(interval);
-}
-
-double get_a(const Vector *vector)
-{
-	return vector->a;
-}
-
-double get_b(const Vector *vector)
-{
-	return vector->b;
-}
-
-double get_c(const Vector *vector)
-{
-	return vector->c;
-}
-
-double get_alpha(const Vector *vector)
-{
-	return vector->alpha;
-}
-
-double get_beta(const Vector *vector)
-{
-	return vector->beta;
-}
-
-double get_x0(const Vector *vector)
-{
-	return vector->x0;
-}
-
-double get_y0(const Vector *vector)
-{
-	return vector->y0;
-}
-
-int get_quantity_of_points(const Interval *interval)
-{
-	return interval->quantity_of_points;
-}
-
-double get_h(const Interval *interval)
-{
-	return interval->h;
-}
-
-int get_quantity_of_jumps(const Interval *interval)
-{
-	return interval->quantity_of_jumps;
-}
-
-double get_distance_jump(const Interval *interval)
-{
-	return interval->distance_jump;
-}
-
-void set_a(Vector *vector, double a)
-{
-	vector->a = a;
-}
-
-void set_b(Vector *vector, double b)
-{
-	vector->b = b;
-}
-
-void set_c(Vector *vector, double c)
-{
-	vector->c = c;
-}
-
-void set_alpha(Vector *vector, double alpha)
-{
-	vector->alpha = alpha;
-}
-
-void set_beta(Vector *vector, double beta)
-{
-	vector->beta = beta;
-}
-
-void set_x0(Vector *vector, double x0)
-{
-	vector->x0 = x0;
-}
-
-void set_y0(Vector *vector, double y0)
-{
-	vector->y0 = y0;
-}
-
-int set_quantity_of_points(Interval *interval, int quantity)
-{
-	interval->quantity_of_points = quantity;
-}
-
-void set_h(Interval *interval, double h)
-{
-	interval->h = h;
-}
-
-void set_quantity_of_jumps(Interval *interval, int quantity_of_jumps)
-{
-	interval->quantity_of_jumps = quantity_of_jumps;
-}
-
-void set_distance_jump(Interval *interval, double distance_jump)
-{
-	interval->distance_jump = distance_jump;
 }
