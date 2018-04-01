636552346595d706ad2ef0b129a5878794aa2ff9
Egor Anikin <egor-anikin@inbox.ru>
2018-04-02 00:15:39 +0300

    Correction solutions of equations

--- Differential_equation/include/SpecialPoints.h
+++ Differential_equation/include/SpecialPoints.h
@@ -5,13 +5,13 @@ typedef struct SpecialPoint SpecialPoint;
 
 SpecialPoint *create_SpecialPoint(Vector *vector);
 
-const float get_x0(SpecialPoint *sPoint);
-const float get_x1(SpecialPoint *sPoint);
-const float get_x2(SpecialPoint *sPoint);
-const float get_x3(SpecialPoint *sPoint);
-const float get_y0(SpecialPoint *sPoint);
-const float get_y1(SpecialPoint *sPoint);
-const float get_y2(SpecialPoint *sPoint);
-const float get_y3(SpecialPoint *sPoint);
+const double get_x0(SpecialPoint *sPoint);
+const double get_x1(SpecialPoint *sPoint);
+const double get_x2(SpecialPoint *sPoint);
+const double get_x3(SpecialPoint *sPoint);
+const double get_y0(SpecialPoint *sPoint);
+const double get_y1(SpecialPoint *sPoint);
+const double get_y2(SpecialPoint *sPoint);
+const double get_y3(SpecialPoint *sPoint);
 const int get_numbers(SpecialPoint *sPoint);
 
--- Differential_equation/src/SpecialPoins.c
+++ Differential_equation/src/SpecialPoins.c
@@ -3,189 +3,177 @@
 #include <stdlib.h>
 #include <math.h>
 
-#define eps 0.000005
+#define EPS 0.0000000005
+
+typedef struct Point
+{
+    double x;
+    double y;
+} Point;
 
 typedef struct SpecialPoint
 {
-    float x0;
-    float y0;
+    double x0;
+    double y0;
 
-    float x1;
-    float y1;
+    double x1;
+    double y1;
 
-    float x2;
-    float y2;
+    double x2;
+    double y2;
 
-    float x3;
-    float y3;
+    double x3;
+    double y3;
 
     int numbers;
 } SpecialPoint;
 
-
-
-
-
-int two_solution(float x1, float x2, SpecialPoint *sPoint)
+int three_solution(double x1, double x2, double x3, SpecialPoint *sPoint)
 {
     int i = 1;
-    if(fabsf(x1 + 1)> eps && fabsf(x1)> eps )
+    if (fabs(x1 + 1) > EPS && fabs(x1) > EPS)
     {
-        sPoint->x1 = x1;
-        sPoint->y1 = -(x1 * x1) / (x1 + 1);
+        set_x1(sPoint, x1);
+        set_y1(sPoint, -(x1 * x1) / (x1 + 1));
         i++;
 
-        if(fabsf(x2 + 1)> eps && fabsf(x2)> eps )
+        if (fabs(x2 + 1) > EPS && fabs(x2) > EPS)
         {
-            sPoint->x2 = x2;
-            sPoint->y2 = -(x2 * x2) / (x2 + 1);
-            i++;
-        }
-    }
-    else if (fabsf(x2 + 1)> eps && fabsf(x2)> eps )
-    {
-        sPoint->x1 = x2;
-        sPoint->y1 = -(x2 * x2) / (x2 + 1);
-        i++;
-    }
-
-    return i;
-}
-
-int three_solution(float x1, float x2, float x3, SpecialPoint *sPoint)
-{
-    int i = 1;
-    if(fabsf(x1 + 1)> eps && fabsf(x1)> eps )
-    {
-        sPoint->x1 = x1;
-        sPoint->y1 = -(x1 * x1) / (x1 + 1);
-        i++;
-
-        if(fabsf(x2 + 1)> eps && fabsf(x2)> eps )
-        {
-            sPoint->x2 = x2;
-            sPoint->y2 = -(x2 * x2) / (x2 + 1);
+            set_x2(sPoint, x2);
+            set_y2(sPoint, -(x2 * x2) / (x2 + 1));
             i++;
 
-            if(fabsf(x3 + 1)> eps && fabsf(x3)> eps )
+            if (fabs(x3 + 1) > EPS && fabs(x3) > EPS)
             {
-                sPoint->x3 = x2;
-                sPoint->y3 = -(x3 * x3) / (x3 + 1);
+                set_x3(sPoint, x3);
+                set_y3(sPoint, -(x3 * x3) / (x3 + 1));
                 i++;
             }
 
         }
-        else if (fabsf(x3 + 1)> eps && fabsf(x3)> eps )
+        else if (fabs(x3 + 1) > EPS && fabs(x3) > EPS)
         {
-            sPoint->x2 = x3;
-            sPoint->y2 = -(x3 * x3) / (x3 + 1);
+            set_x2(sPoint, x3);
+            set_y2(sPoint, -(x3 * x3) / (x3 + 1));
             i++;
         }
     }
-    else if (fabsf(x2 + 1)> eps && fabsf(x2)> eps )
+    else if (fabs(x2 + 1) > EPS && fabs(x2) > EPS)
     {
-        sPoint->x1 = x2;
-        sPoint->y1 = -(x2 * x2) / (x2 + 1);
+        set_x1(sPoint, x2);
+        set_y1(sPoint, -(x2 * x2) / (x2 + 1));
         i++;
 
-        if(fabsf(x3 + 1)> eps && fabsf(x3)> eps )
+        if (fabs(x3 + 1) > EPS && fabs(x3) > EPS)
         {
-            sPoint->x2 = x3;
-            sPoint->y2 = -(x3 * x3) / (x3 + 1);
+            set_x2(sPoint, x3);
+            set_y2(sPoint, -(x3 * x3) / (x3 + 1));
             i++;
         }
     }
-    else if (fabsf(x3 + 1)> eps && fabsf(x3)> eps )
+    else if (fabs(x3 + 1) > EPS && fabs(x3) > EPS)
     {
-        sPoint->x1 = x3;
-        sPoint->y1 = -(x3 * x3) / (x3 + 1);
+        set_x1(sPoint, x3);
+        set_y1(sPoint, -(x3 * x3) / (x3 + 1));
         i++;
     }
 
     return i;
 }
 
-int  solve_cubic_eqation(const float A, const float B, const float C, const float D, SpecialPoint *sPoint)
-{
-    const float p = C / A - (B * B) / (3 * A * A);
-    const float q = 2 * B * B * B / (27 * A * A * A) -  (B * C) / (3 * A * A) + D / A;
-    const float Q1 = (p * p * p/ 27) + (q * q / 4);
-
-    if (fabsf(Q1)<eps)
+int two_solution(double x1, double x2, SpecialPoint *sPoint) {
+    int i = 1;
+    if (fabs(x1 + 1) > EPS && fabs(x1) > EPS)
     {
-        const float alpha = cbrtf(- q / 2);
-        const float x1 = 2 * alpha - B / (3 * A);
-        const float x2 = - alpha - B / (3 * A);
+        set_x1(sPoint, x1);
+        set_y1(sPoint, -(x1 * x1) / (x1 + 1));
+        i++;
 
-        return two_solution( x1, x2, sPoint);
+        if (fabs(x2 + 1) > EPS && fabs(x2) > EPS)
+        {
+            set_x2(sPoint, x2);
+            set_y2(sPoint, -(x2 * x2) / (x2 + 1));
+            i++;
+        }
+    } else if (fabs(x2 + 1) > EPS && fabs(x2) > EPS)
+    {
+        set_x1(sPoint, x2);
+        set_y1(sPoint, -(x2 * x2) / (x2 + 1));
+        i++;
+    }
+
+    return i;
+}
+
+int  solve_cubic_eqation(const double A, const double B, const double C, const double D, SpecialPoint *sPoint)
+{
+    const double p = C / A - (B * B) / (3 * A * A);
+    const double q = 2 * B * B * B / (27 * A * A * A) - (B * C) / (3 * A * A) + D / A;
+    const double Q1 = (p * p * p / 27) + (q * q / 4);
+
+    if (fabs(Q1) < EPS)
+    {
+        const double alpha = cbrt(-q / 2);
+        const double x1 = 2 * alpha - B / (3 * A);
+        const double x2 = -alpha - B / (3 * A);
+
+        return two_solution(x1, x2, sPoint);
     }
     else if (Q1 > 0)
     {
-        const float alpha = cbrtf(- q / 2 + sqrtf(Q1));
-        const float beta = cbrtf(- q / 2 - sqrtf(Q1));
-        const float x = alpha + beta - B / (3 * A);
+        const double alpha = cbrt(-q / 2 + sqrt(Q1));
+        const double beta = cbrt(-q / 2 - sqrt(Q1));
+        const double x = alpha + beta - B / (3 * A);
 
-        if (fabsf(x + 1) > eps && fabsf(x)> eps)
+        if (fabs(x + 1) > EPS && fabs(x) > EPS)
         {
-            sPoint->x1 = x;
-            sPoint->y1 = -(x * x) / (x + 1);
+            set_x1(sPoint, x);
+            set_y1(sPoint, -(x * x) / (x + 1));
+
             return 2;
         }
         else
         {
-
             return 1;
         }
     }
     else
     {
+        const double r = sqrt(-p * p * p / 27);
+        const double fi = acos(-q / (2 * p)) / 3;
 
-        const float ImQ = sqrtf(-Q1);
-        const float Ralpha =  sqrtf(q * q / 4 + Q1);
-        const float Falpha =  atanf (ImQ /( q * q / 4)) / 3;
-        const float ReAlpha = cbrtf(Ralpha) * cosf(Falpha);
-        const float ImAlpha = cbrtf(Ralpha) * sinf(Falpha);
-
-        const float Rbeta =  Ralpha;
-        const float Fbeta =  atanf (-ImQ /( q * q / 4)) / 3;
-        const float ReBeta = cbrtf(Rbeta) * cosf(Fbeta);
-        const float ImBeta = cbrtf(Rbeta) * sinf(Fbeta);
-
-        const float x1 = ReAlpha + ReBeta - B / (3 * A);
-        const float x2 = - (ReAlpha + ReBeta) / 2 - ((ImAlpha - ImBeta)*sqrtf(3)/2)- B / (3 * A);
-        const float x3 = - (ReAlpha + ReBeta) / 2 + ((ImAlpha - ImBeta)*sqrtf(3)/2)- B / (3 * A);
+        const double x1 = 2 * cbrt(r) * cos(fi) - B / (3 * A);
+        const double x2 = 2 * cbrt(r) * cos(fi + 2 * M_PI / 3) - B / (3 * A);
+        const double x3 = 2 * cbrt(r) * cos(fi + 4 * M_PI / 3) - B / (3 * A);
 
         return three_solution(x1, x2, x3, sPoint);
-
     }
 }
 
-
-
-int solve_square_eqation( const float A, const float B, const float C, SpecialPoint *sPoint)
+int solve_square_eqation( const double A, const double B, const double C, SpecialPoint *sPoint)
 {
-    const float Dis = B * B - 4 * A * C;
-    if (fabsf(Dis)<eps)
+    const double Dis = B * B - 4 * A * C;
+    if (fabs(Dis) < EPS)
     {
-        const float  x = - B/(2*A);
-        if (fabsf(x + 1) > eps && fabsf(x)> eps)
+        const double x = -B / (2 * A);
+        if (fabs(x + 1) > EPS && fabs(x) > EPS)
         {
-            sPoint->x1 = x;
-            sPoint->y1 = -(x * x) / (x + 1);
+            set_x1(sPoint, x);
+            set_y1(sPoint, -(x * x) / (x + 1));
+
             return 2;
         }
         else
         {
-
             return 1;
         }
     }
-    if (Dis >0)
+    if (Dis > 0)
     {
-        const float  x1 = ( - B + sqrtf(Dis))/ (2*A);
-        const float  x2 = ( - B - sqrtf(Dis))/ (2*A);
+        const double x1 = (-B + sqrt(Dis)) / (2 * A);
+        const double x2 = (-B - sqrt(Dis)) / (2 * A);
 
-        return two_solution( x1, x2, sPoint);
+        return two_solution(x1, x2, sPoint);
     }
     else
     {
@@ -193,13 +181,13 @@ int solve_square_eqation( const float A, const float B, const float C, SpecialPo
     }
 }
 
-int solve_linear_eqation( const float A, const float B, SpecialPoint *sPoint)
+int solve_linear_eqation( const double A, const double B, SpecialPoint *sPoint)
 {
-    const float  x = - B / A;
-    if (fabsf(x - 1) > eps && fabsf(x)> eps)
+    const double x = -B / A;
+    if (fabs(x - 1) > EPS && fabs(x) > EPS)
     {
-        sPoint->x1 = x;
-        sPoint->y1 = -(x * x) / (x + 1);
+        set_x1(sPoint, x);
+        set_x1(sPoint, -(x * x) / (x + 1));
         return 2;
     }
     else
@@ -208,37 +196,36 @@ int solve_linear_eqation( const float A, const float B, SpecialPoint *sPoint)
     }
 }
 
-
 int num ( Vector *vector, SpecialPoint *sPoint)
 {
-    const float a = get_a(vector);
-    const float b = get_b(vector);
-    const float c = get_c(vector);
-    const float alpha = get_alpha(vector);
-    const float beta = get_beta(vector);
+    const double a = get_a(vector);
+    const double b = get_b(vector);
+    const double c = get_c(vector);
+    const double alpha = get_alpha(vector);
+    const double beta = get_beta(vector);
 
-    const float A1 = a - b + c;
-    const float B1 = 2*a - b + alpha - beta;
-    const float C1 = a - beta + 2*alpha;
-    const float D1 = alpha;
+    const double A1 = a - b + c;
+    const double B1 = 2 * a - b + alpha - beta;
+    const double C1 = a - beta + 2 * alpha;
+    const double D1 = alpha;
 
     if (A1 != 0)
     {
-        return  solve_cubic_eqation(A1, B1, C1, D1, sPoint);
+        return solve_cubic_eqation(A1, B1, C1, D1, sPoint);
     }
 
     if (B1 != 0)
     {
-        return  solve_square_eqation( B1, C1, D1, sPoint);
+        return solve_square_eqation(B1, C1, D1, sPoint);
     }
 
     if (C1 != 0)
     {
-        return  solve_linear_eqation(C1, D1, sPoint);
+        return solve_linear_eqation(C1, D1, sPoint);
     }
     if (D1 != 0)
     {
-        return  1;
+        return 1;
     }
     else
     {
@@ -250,55 +237,100 @@ SpecialPoint *create_SpecialPoint(Vector *vector)
 {
     SpecialPoint *sPoint = malloc(sizeof(SpecialPoint));
 
-    sPoint->x0 = 0;
-    sPoint->y0 = 0;
+    set_x0(sPoint, 0);
+    set_y0(sPoint, 0);
 
-    sPoint->numbers = num(vector, sPoint);
+    set_numbers(sPoint, num(vector, sPoint));
     return sPoint;
 }
 
-
-const float get_x0(SpecialPoint *sPoint)
+const double get_x0(SpecialPoint *sPoint)
 {
-    return sPoint->x0 ;
+    return sPoint->x0;
 }
 
-const float get_x1(SpecialPoint *sPoint)
+const double get_x1(SpecialPoint *sPoint)
 {
-    return sPoint->x1 ;
+    return sPoint->x1;
 }
 
-const float get_x2(SpecialPoint *sPoint)
+const double get_x2(SpecialPoint *sPoint)
 {
-    return sPoint->x2 ;
+    return sPoint->x2;
 }
 
-const float get_x3(SpecialPoint *sPoint)
+const double get_x3(SpecialPoint *sPoint)
 {
-    return sPoint->x3 ;
+    return sPoint->x3;
 }
 
-const float get_y0(SpecialPoint *sPoint)
+const double get_y0(SpecialPoint *sPoint)
 {
-    return sPoint->y0 ;
+    return sPoint->y0;
 }
 
-const float get_y1(SpecialPoint *sPoint)
+const double get_y1(SpecialPoint *sPoint)
 {
-    return sPoint->y1 ;
+    return sPoint->y1;
 }
 
-const float get_y2(SpecialPoint *sPoint)
+const double get_y2(SpecialPoint *sPoint)
 {
-    return sPoint->y2 ;
+    return sPoint->y2;
 }
 
-const float get_y3(SpecialPoint *sPoint)
+const double get_y3(SpecialPoint *sPoint)
 {
-    return sPoint->y3 ;
+    return sPoint->y3;
 }
 
 const int  get_numbers(SpecialPoint *sPoint)
 {
-    return sPoint->numbers ;
+    return sPoint->numbers;
 }
+
+void set_x0(SpecialPoint *sPoint, double x0)
+{
+    sPoint->x0 = x0;
+}
+
+void set_x1(SpecialPoint *sPoint, double x1)
+{
+    sPoint->x1 = x1;
+}
+
+void set_x2(SpecialPoint *sPoint, double x2)
+{
+    sPoint->x2 = x2;
+}
+
+void set_x3(SpecialPoint *sPoint, double x3)
+{
+    sPoint->x3 = x3;
+}
+
+void set_y0(SpecialPoint *sPoint, double y0)
+{
+    sPoint->y0 = y0;
+}
+
+void set_y1(SpecialPoint *sPoint, double y1)
+{
+    sPoint->y1 = y1;
+}
+
+void set_y2(SpecialPoint *sPoint, double y2)
+{
+    sPoint->y2 = y2;
+}
+
+void set_y3(SpecialPoint *sPoint, double y3)
+{
+    sPoint->y3 = y3;
+}
+
+void set_numbers(SpecialPoint *sPoint, int numbers)
+{
+    sPoint->numbers = numbers;
+}
+
